63c4ae7289df25592b52effdc94671ca26d06d48
abhishek2813 <abhi93158879@gmail.com>
2023-01-14 18:56:37 +0530

    first commit

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ function chcekform(){
     var age = document.getElementById("Age").value
     var flag = false;
     if(name !="" && Profession !="" && age !=""){
+        flag=true;
         document.getElementById("success-msg").classList.remove("succuss")
         document.getElementById("error-msg").classList.add("error")
-        flag=true;
+        
     }else{
         document.getElementById("error-msg").classList.remove("error")
+        document.getElementById("success-msg").classList.add("succuss")
     }
+
     let table=document.getElementById('data');
     if(flag){
-        document.getElementById("tab").classList.add("success")
+        document.getElementById("tab").classList.add("dis")
         document.getElementById("data").classList.remove("success")
     }
         let newArray = ["Name :"+" "+name,"Profession"+" "+Profession,"Age"+" "+age,"Delete User"];
